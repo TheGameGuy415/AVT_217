aa424f128419568a14d2feb45f4a2ca2e57ce690
Connor Ryan <connorjr15@gmail.com>
2019-04-04 13:12:37 -0400

    e16 progress

--- e16/main.js
+++ e16/main.js
@@ -3,17 +3,17 @@ $(document).ready(function()
     $('#listName').mouseenter(function()
       {
         $('#listName').addClass('blue');
-        $('#title').css('color', 'white');
+        $('#listTitle').css('color', 'white');
       });
     $('#listName').mouseleave(function()
       {
         $('#listName').removeClass('blue');
-        $('#title').css('color', 'black');
+        $('#listTitle').css('color', 'black');
       });
 
     $('#g1Title').click(function()
       {
-        $('#game1').toggleClass('blue');
+        $('#game1').toggleClass('closed');
         $('#game1').toggleClass('open');
       });
 
