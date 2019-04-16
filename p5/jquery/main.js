b978fb6e1e37b7a032345877f1beff466f609679
Connor Ryan <connorjr15@gmail.com>
2019-04-16 06:06:20 -0400

    Project 5 prototype

---
+++ p5/jquery/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function()
+  {
+      // run function on initial page load
+      jokes();
+  });
+
+function jokes()
+  {
+    var jokesArray = ["Joke1","Joke2","Joke3","Joke4","Joke5","Joke6"]
+    var pnchlineArray = ["pnchline1","pnchline2","pnchline3","pnchline4","pnchline5","pnchline6"]
+  	var index = Math.floor(Math.random() * jokesArray.length);
+    console.log(index);
+    console.log(jokesArray[index]);
+    console.log(pnchlineArray[index]);
+
+    $('#joke').text(jokesArray[index]);
+    $('#pnchline').text(pnchlineArray[index]);
+
+    $('#button').click(function()
+      {
+        $('#pnchline').css('opacity', '1');
+      });
+  }
