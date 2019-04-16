6cf580c2b66953658c55dfc5baf066d7539c42dd
Connor Ryan <connorjr15@gmail.com>
2019-04-16 13:04:26 -0400

    p5 update

--- p5/jquery/main.js
+++ p5/jquery/main.js
@@ -6,9 +6,12 @@ $(document).ready(function()
 
 function jokes()
   {
-    var jokesArray = ["Joke1","Joke2","Joke3","Joke4","Joke5","Joke6"]
+    // $('#pnchline').addClass('hidden');
+
+    var jokesArray = ["joke1","joke2","joke3","joke4","joke5","joke6"]
     var pnchlineArray = ["pnchline1","pnchline2","pnchline3","pnchline4","pnchline5","pnchline6"]
   	var index = Math.floor(Math.random() * jokesArray.length);
+
     console.log(index);
     console.log(jokesArray[index]);
     console.log(pnchlineArray[index]);
@@ -16,10 +19,28 @@ function jokes()
     $('#joke').text(jokesArray[index]);
     $('#pnchline').text(pnchlineArray[index]);
 
+    var shown = false;
     $('#button').click(function()
       {
-        $('#pnchline').css('opacity', '1');
+        if (!shown)
+          {
+            $('#pnchline').removeClass('hidden');
+            $('#pnchline').addClass('shown');
+            $('#button').text('Give me another one!');
+
+            shown = true;
+          }
+        else
+          {
+            $('#pnchline').removeClass('shown');
+            $('#pnchline').addClass('hidden');
+            $('#button').text('punchline prompt');
+
+            index = Math.floor(Math.random() * jokesArray.length);
+            $('#joke').text(jokesArray[index]);
+            $('#pnchline').text(pnchlineArray[index]);
+
+            shown = false;
+          }
       });
   }
-
-//Maybe re use button to reset the site, infinite jokes!
